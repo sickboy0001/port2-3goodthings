2a8d8a0d6d2e92d4121d615962b11d40550f2ba8
tfujiki <syunjyu0001@gmail.com>
2023-05-11 08:33:04 +0900

    Notion　Title以外のTextも画面表示、日付についてWeekday、WeekEnd色分け

--- src/components/CalendarDay.jsx
+++ src/components/CalendarDay.jsx
@@ -11,26 +11,34 @@ export function CalendarDay(props)  {
     props.date.getDay() === 0 ? "text-red-600" : 
     (props.date.getDay() === 6 ? "text-blue-600" : "text-gray-500");
   // console.log(props.event);
+  const datebgcolor = 
+    props.date.getDay() === 0 ? "bg-red-100" : 
+    (props.date.getDay() === 6 ? "bg-blue-100" : "bg-sky-100");
+  // console.log(props.event);
   return (
   <div key={datestring}>
       <div>
       
-        <p className={`border border-gray-400 rounded-lg shadow-md m-0.5 dark:text-gray-400 font-semibold ${dateforecolor}`}>
+        <div className={`border border-gray-100 shadow-lg m-0.5 dark:text-gray-400 font-semibold bg-sky-50 ${dateforecolor} ${datebgcolor}`}>
           {datestring}
-        </p>
-      </div>
-      <div>
-        {
-          props.event.map((thisevent,index)=>{
-            //todo:components化推奨１
+          <div>
+        {props.event.map((thisevent,index)=>{
+            // var test = [thisevent.properties.Thing.title...thisevent.properties.Text.rich_text]
+            // var test = [thisevent.properties.Thing.title]
+            // console.log(test)
+            // var test = [...thisevent.properties.Thing.title , ...thisevent.properties.Text.rich_text]
+            // console.log(test)
+            
             return(
-              <p key={index} className="border border-gray-400 rounded-lg shadow-md p-1 m-0.5 text-gray-600 dark:text-gray-400 text-left">
-                <NotionText text={thisevent.properties.Thing.title} />
+              <p key={index} className="border border-gray-400 rounded-lg shadow-md p-1 m-0.5 bg-white text-gray-600 dark:text-gray-400 text-left">
+                <NotionText text={[...thisevent.properties.Thing.title , ...thisevent.properties.Text.rich_text] } />
               </p>
               )
             }
           )
         }
+      </div>
+        </div>
       </div>
     </div>
   )
